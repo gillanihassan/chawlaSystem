a67f15d2d82c996f790b467fc97ed27014296362
gillanihassan <hassangillani397@gmail.com>
2025-07-03 17:26:37 +0500

    solution page content block update

--- src/assets/pages/solutions/businessIntelligence/contentBlock/ContentBlock.jsx
+++ src/assets/pages/solutions/businessIntelligence/contentBlock/ContentBlock.jsx
@@ -1,9 +1,27 @@
-import React from "react";
+import { Col, Row } from "react-bootstrap";
+import styles from "./styles.module.css";
 
 function ContentBlock() {
   return (
-    <div>
-      <h2>This is a Content Block</h2>
+    <div className={`container-xxl mt-3 p-3 ${styles.mainContainer}`}>
+      <Row>
+        <Col md={8}>
+          <h2 className={`${styles.topHeading}`}>
+            Business Intelligence Best Practices – The AZK AI Advantage
+          </h2>
+          <p className={`mt-5 ${styles.para}`}>
+            Handling vast, complex datasets comes with its challenges – data
+            congestion, silos, integrity issues, and limited in-house expertise.
+            AZK AI’s Business Intelligence solutions are designed to help you
+            overcome these obstacles with ease.
+          </p>
+          <p className={`mt-3 ${styles.para}`}>
+            What sets us apart? Our unwavering commitment to innovation. We
+            don’t just follow best practices – we redefine them, delivering
+            forward-thinking BI strategies that drive real results.
+          </p>
+        </Col>
+      </Row>
     </div>
   );
 }
